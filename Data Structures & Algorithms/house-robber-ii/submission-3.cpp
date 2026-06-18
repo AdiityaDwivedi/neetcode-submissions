adfd4605644c0f72f18307f045e372e2937e1160
Aditya Dwivedi <aditya22dwivedi22@gmail.com>
2026-06-18 12:02:02 +0530

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.cpp
@@ -0,0 +1,27 @@
+#include<cstring>
+class Solution {
+public:
+    int dp[101];
+    int solve(vector<int>& nums, int i, int n) {
+        if(i >= n) return 0;
+        if(dp[i] != -1) return dp[i];
+
+        int take = nums[i] + solve(nums, i+2, n);
+        int deny = solve(nums, i+1, n);
+
+        return dp[i] = max(take, deny);
+    } 
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+
+        memset(dp, -1, sizeof(dp));
+        int excludeLast = solve(nums, 0, n-1);
+
+        memset(dp, -1, sizeof(dp));
+        int excludeFirst = solve(nums, 1, n);
+
+        return max(excludeLast, excludeFirst);
+    }
+};
